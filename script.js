743d817b63dabcbd1422e24b97bd225f5b7e2189
anna liang <annajliang@gmail.com>
2020-03-19 18:12:56 -0400

    add sweetalert popup

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //where all functions will be stored (namespacing)
 const milhouseApp = {};
 
-milhouseApp.init = function() {
+milhouseApp.init = function () {
+    milhouseApp.alert();
     milhouseApp.showFocusOutline();
     milhouseApp.playSound();
     milhouseApp.pauseSound();
@@ -9,32 +10,43 @@ milhouseApp.init = function() {
 
 const $sounds = $("audio");
 
-milhouseApp.showFocusOutline = function() {
-    $("body").on("keyup", function(e) {
+milhouseApp.alert = function () {
+    Swal.fire({
+        title: "Quote Info",
+        html: `This quote is from: <br><a href="https://en.wikipedia.org/wiki/Burns%27_Heir">Burns' Heir (Season 5, Episode 18)</a>`,
+        // html: `This quote is from Burns' Heir (Season 5, Episode 18) <iframe width="450" height="300" src="https://www.youtube.com/embed/_bj7AiQiszM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+        icon: "info",
+        padding: "1.5rem",
+        width: "20rem"
+    });
+}
+
+milhouseApp.showFocusOutline = function () {
+    $("body").on("keyup", function (e) {
         if (e.which === 9) {
             $(".button").removeClass("no-focus-outline");
         }
     })
 };
 
-milhouseApp.playSound = function() {
+milhouseApp.playSound = function () {
     for (let i = 0; i < $sounds.length; i++) {
-        $(`.button-${i}`).on("click", function() {
+        $(`.button-${i}`).on("click", function () {
             $sounds[i].play();
         })
     }
 };
 
-milhouseApp.pauseSound = function() {
-    $sounds.on("play", function() {
-        $sounds.not($(this)).each(function(index, sound) {
+milhouseApp.pauseSound = function () {
+    $sounds.on("play", function () {
+        $sounds.not($(this)).each(function (index, sound) {
             sound.pause();
         })
     })
 }
 
 //document ready
-$(function() {
+$(function () {
     //app is called when document is ready
     milhouseApp.init();
 });
