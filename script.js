001061d8987cf1b0e05adf6106b71569799be249
anna liang <annajliang@gmail.com>
2020-03-20 00:14:14 -0400

    implement popups to all buttons

--- script.js
+++ script.js
@@ -18,102 +18,110 @@ const quoteInfo = [
         episodeNum: 18,
     },
     {
-        wikiURL: "https://en.wikipedia.org/wiki/Homerpalooza' target='_blank",
+        wikiURL: "https://en.wikipedia.org/wiki/Homerpalooza",
         episodeName: "Homerpalooza",
         seasonNum: 7,
         episodeNum: 24,
     },
     {
-        wikiURL: "https://en.wikipedia.org/wiki/Marge_Be_Not_Proud' target='_blank",
+        wikiURL: "https://en.wikipedia.org/wiki/Marge_Be_Not_Proud",
         episodeName: "Marge Be Not Proud",
         seasonNum: 7,
         episodeNum: 11,
     },
     {
-        wikiURL: "https://en.wikipedia.org/wiki/Lisa%27s_Sax' target='_blank",
+        wikiURL: "https://en.wikipedia.org/wiki/Lisa%27s_Sax",
         episodeName: "Lisa's Sax",
         seasonNum: 9,
         episodeNum: 3,
     },
     {
-        wikiURL: "https://en.wikipedia.org/wiki/Bart_Sells_His_Soul' target='_blank",
+        wikiURL: "https://en.wikipedia.org/wiki/Bart_Sells_His_Soul",
         episodeName: "Bart Sells His Soul",
         seasonNum: 7,
         episodeNum: 4,
     },
     {
-        wikiURL: "https://en.wikipedia.org/wiki/Lemon_of_Troy' target='_blank",
+        wikiURL: "https://en.wikipedia.org/wiki/Lemon_of_Troy",
         episodeName: "Lemon of Troy",
         seasonNum: 6,
         episodeNum: 24,
     },
     {
-        wikiURL: "https://en.wikipedia.org/wiki/Lisa%27s_Date_with_Density' target='_blank",
+        wikiURL: "https://en.wikipedia.org/wiki/Lisa%27s_Date_with_Density",
         episodeName: "Lisa's Date with Density",
         seasonNum: 8,
         episodeNum: 7,
     },
     {
-        wikiURL: "https://en.wikipedia.org/wiki/Lemon_of_Troy' target='_blank",
+        wikiURL: "https://en.wikipedia.org/wiki/Brother_from_the_Same_Planet",
+        episodeName: "Brother from the Same Planet",
+        seasonNum: 4,
+        episodeNum: 14,
+    },
+    {
+        wikiURL: "https://en.wikipedia.org/wiki/Lemon_of_Troy",
         episodeName: "Lemon of Troy",
         seasonNum: 6,
         episodeNum: 24,
     },
     {
-        wikiURL: "https://en.wikipedia.org/wiki/The_Canine_Mutiny' target='_blank",
+        wikiURL: "https://en.wikipedia.org/wiki/The_Canine_Mutiny",
         episodeName: "The Canine Mutiny",
         seasonNum: 8,
         episodeNum: 20,
     },
     {
-        wikiURL: "https://en.wikipedia.org/wiki/Lemon_of_Troy' target='_blank",
+        wikiURL: "https://en.wikipedia.org/wiki/Lemon_of_Troy",
         episodeName: "Lemon of Troy",
         seasonNum: 6,
         episodeNum: 24,
     },
     {
-        wikiURL: "https://en.wikipedia.org/wiki/Lisa%27s_Date_with_Density' target='_blank",
+        wikiURL: "https://en.wikipedia.org/wiki/Lisa%27s_Date_with_Density",
         episodeName: "Lisa's Date with Density",
         seasonNum: 8,
         episodeNum: 7,
     },
     {
-        wikiURL: "https://en.wikipedia.org/wiki/Mom_and_Pop_Art' target='_blank",
+        wikiURL: "https://en.wikipedia.org/wiki/Mom_and_Pop_Art",
         episodeName: "Mom and Pop Art",
         seasonNum: 10,
         episodeNum: 19,
     },
     {
-        wikiURL: "https://en.wikipedia.org/wiki/Team_Homer' target='_blank",
-        episodeName: "Mom and Pop Art",
+        wikiURL: "https://en.wikipedia.org/wiki/Team_Homer",
+        episodeName: "Team Homer",
         seasonNum: 7,
         episodeNum: 12,
     },
     {
-        wikiURL: "https://en.wikipedia.org/wiki/Days_of_Future_Future' target='_blank",
+        wikiURL: "https://en.wikipedia.org/wiki/Days_of_Future_Future",
         episodeName: "Days of Future Future",
         seasonNum: 25,
         episodeNum: 18,
     },
     {
-        wikiURL: "https://en.wikipedia.org/wiki/Sideshow_Bob%27s_Last_Gleaming' target='_blank",
-        episodeName: "Days of Future Future",
+        wikiURL: "https://en.wikipedia.org/wiki/Sideshow_Bob%27s_Last_Gleaming",
+        episodeName: "Sideshow Bob's Last Gleaming",
         seasonNum: 7,
         episodeNum: 9,
     }
 ]
 
 milhouseApp.alert = function () {
-    $("i").on("click", function() {
-        Swal.fire({
-            title: "Info",
-            html: `This audio is from: <br><a href="${quoteInfo[0].wikiURL}" target="_blank">${quoteInfo[0].episodeName} (Season ${quoteInfo[0].seasonNum}, Episode ${quoteInfo[0].episodeNum})</a>`,
-            // html: `This quote is from Burns' Heir (Season 5, Episode 18) <iframe width="450" height="300" src="https://www.youtube.com/embed/_bj7AiQiszM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
-            icon: "info",
-            padding: "1.5rem",
-            width: "20rem"
-        });       
-    })
+    for (let i = 0; i < quoteInfo.length; i++) {
+        $(`.icon-${i+1}`).on("click", function () {
+            Swal.fire({
+                title: "Info",
+                html: `This audio is from: <br><a href="${quoteInfo[i].wikiURL}" target="_blank">${quoteInfo[i].episodeName} <br>(Season ${quoteInfo[i].seasonNum}, Episode ${quoteInfo[i].episodeNum})</a>`,
+                // html: `This quote is from Burns' Heir (Season 5, Episode 18) <iframe width="450" height="300" src="https://www.youtube.com/embed/_bj7AiQiszM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+                icon: "info",
+                padding: "1.5rem",
+                width: "20rem"
+            });
+        })
+    }
 }
 
 milhouseApp.showFocusOutline = function () {
