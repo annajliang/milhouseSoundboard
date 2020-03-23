950be91288f78df70ab0b74da0a2d6d59733aca4
anna liang <annajliang@gmail.com>
2020-03-23 15:07:43 -0400

    comment out responsive test code of script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const milhouseApp = {};
 
 milhouseApp.init = function () {
     milhouseApp.infoAlert();
-    milhouseApp.infoAlertMobile();
+    // milhouseApp.infoAlertMobile();
     milhouseApp.showFocusOutline();
     milhouseApp.toggleSound();
     milhouseApp.playSingleSound();
@@ -147,28 +147,28 @@ milhouseApp.infoAlert = function () {
     }
 }
 
-milhouseApp.infoAlertMobile = function () {
-    if (matchMedia('only screen and (max-width: 425px)').matches) {
-        for (let i = 0; i < quoteInfo.length; i++) {
-            $(`.icon-${i + 1}`).on("click", function () {
-                Swal.fire({
-                    // title: "Info",
-                    icon: "info",
-                    html: `
-                    This audio is from: 
-                    <br><a href="${quoteInfo[i].wikiURL}" target="_blank">${quoteInfo[i].episodeName}
-                    (Season ${quoteInfo[i].seasonNum}, Episode ${quoteInfo[i].episodeNum})</a>
-                    <br><iframe width="300" height="250" class="padding" src="${quoteInfo[i].youtubeURL}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    `,
-                    showCloseButton: true,
-                    padding: "1.5rem",
-                    // width: "20rem"
-                    width: "25rem"
-                });
-            })
-        }
-    }
-}
+// milhouseApp.infoAlertMobile = function () {
+//     if (matchMedia('only screen and (max-width: 425px)').matches) {
+//         for (let i = 0; i < quoteInfo.length; i++) {
+//             $(`.icon-${i + 1}`).on("click", function () {
+//                 Swal.fire({
+//                     // title: "Info",
+//                     icon: "info",
+//                     html: `
+//                     This audio is from: 
+//                     <br><a href="${quoteInfo[i].wikiURL}" target="_blank">${quoteInfo[i].episodeName}
+//                     (Season ${quoteInfo[i].seasonNum}, Episode ${quoteInfo[i].episodeNum})</a>
+//                     <br><iframe width="300" height="250" class="padding" src="${quoteInfo[i].youtubeURL}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+//                     `,
+//                     showCloseButton: true,
+//                     padding: "1.5rem",
+//                     // width: "20rem"
+//                     width: "25rem"
+//                 });
+//             })
+//         }
+//     }
+// }
 
 milhouseApp.showFocusOutline = function () {
     $("body").on("keyup", function (e) {
