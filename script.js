c9ccb65856716adde89a43e73daebcee228ef34c
anna liang <annajliang@gmail.com>
2020-03-30 02:05:01 -0400

    fix issue with filter applying to info icon on tablet/mobile widths, add refractor code

--- script.js
+++ script.js
@@ -16,116 +16,133 @@ const $sounds = $("audio");
 //variable where all button items are stored
 const $buttons = $(".button__item");
 
+const repeatedInfo = {
+    lemonOfTroy: {
+        wikiUrl: "https://en.wikipedia.org/wiki/Lemon_of_Troy",
+        youtubeUrl: "https://www.youtube.com/embed/UxQsWHQWKsk",
+        episodeName: "Lemon of Troy",
+        seasonNum: 6,
+        episodeNum: 24,
+    },
+    lisasDateWithDensity: {
+        wikiUrl: "https://en.wikipedia.org/wiki/Lisa%27s_Date_with_Density",
+        youtubeUrl: "https://www.youtube.com/embed/WX8cDA87V3Y",
+        episodeName: "Lisa's Date with Density",
+        seasonNum: 8,
+        episodeNum: 7,
+    },
+};
+
 //variable that contains an array of objects that stores additional information regarding the audio files
 const soundInfo = [
     {
-        wikiURL: "https://en.wikipedia.org/wiki/Burns%27_Heir",
-        youtubeURL: "https://www.youtube.com/embed/_bj7AiQiszM",
+        wikiUrl: "https://en.wikipedia.org/wiki/Burns%27_Heir",
+        youtubeUrl: "https://www.youtube.com/embed/_bj7AiQiszM",
         episodeName: "Burns' Heir",
         seasonNum: 5,
         episodeNum: 18,
     },
     {
-        wikiURL: "https://en.wikipedia.org/wiki/Homerpalooza",
-        youtubeURL: "https://www.youtube.com/embed/mA1Mm22y9NM",
+        wikiUrl: "https://en.wikipedia.org/wiki/Homerpalooza",
+        youtubeUrl: "https://www.youtube.com/embed/mA1Mm22y9NM",
         episodeName: "Homerpalooza",
         seasonNum: 7,
         episodeNum: 24,
     },
     {
-        wikiURL: "https://en.wikipedia.org/wiki/Marge_Be_Not_Proud",
-        youtubeURL: "https://www.youtube.com/embed/uUOxUEPJlNw",
+        wikiUrl: "https://en.wikipedia.org/wiki/Marge_Be_Not_Proud",
+        youtubeUrl: "https://www.youtube.com/embed/uUOxUEPJlNw",
         episodeName: "Marge Be Not Proud",
         seasonNum: 7,
         episodeNum: 11,
     },
     {
-        wikiURL: "https://en.wikipedia.org/wiki/Lisa%27s_Sax",
-        youtubeURL: "https://www.youtube.com/embed/8k5D3sP_fqA",
+        wikiUrl: "https://en.wikipedia.org/wiki/Lisa%27s_Sax",
+        youtubeUrl: "https://www.youtube.com/embed/8k5D3sP_fqA",
         episodeName: "Lisa's Sax",
         seasonNum: 9,
         episodeNum: 3,
     },
     {
-        wikiURL: "https://en.wikipedia.org/wiki/Bart_Sells_His_Soul",
-        youtubeURL: "https://www.youtube.com/embed/3dENaxa6NGM",
+        wikiUrl: "https://en.wikipedia.org/wiki/Bart_Sells_His_Soul",
+        youtubeUrl: "https://www.youtube.com/embed/3dENaxa6NGM",
         episodeName: "Bart Sells His Soul",
         seasonNum: 7,
         episodeNum: 4,
     },
     {
-        wikiURL: "https://en.wikipedia.org/wiki/Lemon_of_Troy",
-        youtubeURL: "https://www.youtube.com/embed/UxQsWHQWKsk",
-        episodeName: "Lemon of Troy",
-        seasonNum: 6,
-        episodeNum: 24,
+        wikiUrl: `${repeatedInfo.lemonOfTroy.wikiUrl}`,
+        youtubeUrl: `${repeatedInfo.lemonOfTroy.youtubeUrl}`,
+        episodeName: `${repeatedInfo.lemonOfTroy.episodeName}`,
+        seasonNum: `${repeatedInfo.lemonOfTroy.seasonNum}`,
+        episodeNum: `${repeatedInfo.lemonOfTroy.episodeNum}`,
     },
     {
-        wikiURL: "https://en.wikipedia.org/wiki/Lisa%27s_Date_with_Density",
-        youtubeURL: "https://www.youtube.com/embed/WX8cDA87V3Y",
-        episodeName: "Lisa's Date with Density",
-        seasonNum: 8,
-        episodeNum: 7,
+        wikiUrl: `${repeatedInfo.lisasDateWithDensity.wikiUrl}`,
+        youtubeUrl: `${repeatedInfo.lisasDateWithDensity.youtubeUrl}`,
+        episodeName: `${repeatedInfo.lisasDateWithDensity.episodeName}`,
+        seasonNum: `${repeatedInfo.lisasDateWithDensity.seasonNum}`,
+        episodeNum: `${repeatedInfo.lisasDateWithDensity.episodeNum}`,
     },
     {
-        wikiURL: "https://en.wikipedia.org/wiki/Brother_from_the_Same_Planet",
-        youtubeURL: "https://www.youtube.com/embed/p_vyjk7c-MY",
+        wikiUrl: "https://en.wikipedia.org/wiki/Brother_from_the_Same_Planet",
+        youtubeUrl: "https://www.youtube.com/embed/p_vyjk7c-MY",
         episodeName: "Brother from the Same Planet",
         seasonNum: 4,
         episodeNum: 14,
     },
     {
-        wikiURL: "https://en.wikipedia.org/wiki/Lemon_of_Troy",
-        youtubeURL: "https://www.youtube.com/embed/UxQsWHQWKsk",
-        episodeName: "Lemon of Troy",
-        seasonNum: 6,
-        episodeNum: 24,
+        wikiUrl: `${repeatedInfo.lemonOfTroy.wikiUrl}`,
+        youtubeUrl: `${repeatedInfo.lemonOfTroy.youtubeUrl}`,
+        episodeName: `${repeatedInfo.lemonOfTroy.episodeName}`,
+        seasonNum: `${repeatedInfo.lemonOfTroy.seasonNum}`,
+        episodeNum: `${repeatedInfo.lemonOfTroy.episodeNum}`,
     },
     {
-        wikiURL: "https://en.wikipedia.org/wiki/The_Canine_Mutiny",
-        youtubeURL: "https://www.youtube.com/embed/poyorjOXeCg",
+        wikiUrl: "https://en.wikipedia.org/wiki/The_Canine_Mutiny",
+        youtubeUrl: "https://www.youtube.com/embed/poyorjOXeCg",
         episodeName: "The Canine Mutiny",
         seasonNum: 8,
         episodeNum: 20,
     },
     {
-        wikiURL: "https://en.wikipedia.org/wiki/Lemon_of_Troy",
-        youtubeURL: "https://www.youtube.com/embed/UxQsWHQWKsk",
-        episodeName: "Lemon of Troy",
-        seasonNum: 6,
-        episodeNum: 24,
+        wikiUrl: `${repeatedInfo.lemonOfTroy.wikiUrl}`,
+        youtubeUrl: `${repeatedInfo.lemonOfTroy.youtubeUrl}`,
+        episodeName: `${repeatedInfo.lemonOfTroy.episodeName}`,
+        seasonNum: `${repeatedInfo.lemonOfTroy.seasonNum}`,
+        episodeNum: `${repeatedInfo.lemonOfTroy.episodeNum}`,
     },
     {
-        wikiURL: "https://en.wikipedia.org/wiki/Lisa%27s_Date_with_Density",
-        youtubeURL: "https://www.youtube.com/embed/WX8cDA87V3Y",
-        episodeName: "Lisa's Date with Density",
-        seasonNum: 8,
-        episodeNum: 7,
+        wikiUrl: `${repeatedInfo.lisasDateWithDensity.wikiUrl}`,
+        youtubeUrl: `${repeatedInfo.lisasDateWithDensity.youtubeUrl}`,
+        episodeName: `${repeatedInfo.lisasDateWithDensity.episodeName}`,
+        seasonNum: `${repeatedInfo.lisasDateWithDensity.seasonNum}`,
+        episodeNum: `${repeatedInfo.lisasDateWithDensity.episodeNum}`,
     },
     {
-        wikiURL: "https://en.wikipedia.org/wiki/Mom_and_Pop_Art",
-        youtubeURL: "https://www.youtube.com/embed/dGdX5Bpc1NU",
+        wikiUrl: "https://en.wikipedia.org/wiki/Mom_and_Pop_Art",
+        youtubeUrl: "https://www.youtube.com/embed/dGdX5Bpc1NU",
         episodeName: "Mom and Pop Art",
         seasonNum: 10,
         episodeNum: 19,
     },
     {
-        wikiURL: "https://en.wikipedia.org/wiki/Team_Homer",
-        youtubeURL: "https://www.youtube.com/embed/s332KDgyyf8",
+        wikiUrl: "https://en.wikipedia.org/wiki/Team_Homer",
+        youtubeUrl: "https://www.youtube.com/embed/s332KDgyyf8",
         episodeName: "Team Homer",
         seasonNum: 7,
         episodeNum: 12,
     },
     {
-        wikiURL: "https://en.wikipedia.org/wiki/Days_of_Future_Future",
-        youtubeURL: "https://www.youtube.com/embed/DPBMJrd0O4A",
+        wikiUrl: "https://en.wikipedia.org/wiki/Days_of_Future_Future",
+        youtubeUrl: "https://www.youtube.com/embed/DPBMJrd0O4A",
         episodeName: "Days of Future Future",
         seasonNum: 25,
         episodeNum: 18,
     },
     {
-        wikiURL: "https://en.wikipedia.org/wiki/Sideshow_Bob%27s_Last_Gleaming",
-        youtubeURL: "https://www.youtube.com/embed/fU15E16K01c",
+        wikiUrl: "https://en.wikipedia.org/wiki/Sideshow_Bob%27s_Last_Gleaming",
+        youtubeUrl: "https://www.youtube.com/embed/fU15E16K01c",
         episodeName: "Sideshow Bob's Last Gleaming",
         seasonNum: 7,
         episodeNum: 9,
@@ -147,9 +164,9 @@ soundboardApp.getAlertOptions = function (i, videoWidth, videoHeight, alertPaddi
         icon: "info",
         html: `
                     This audio is from: 
-                    <br><a href="${soundInfo[i].wikiURL}" target="_blank">${soundInfo[i].episodeName}
+                    <br><a href="${soundInfo[i].wikiUrl}" target="_blank">${soundInfo[i].episodeName}
                     (Season ${soundInfo[i].seasonNum}, Episode ${soundInfo[i].episodeNum})</a>
-                    <br><iframe width="${videoWidth}" height="${videoHeight}" class="padding" src="${soundInfo[i].youtubeURL}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <br><iframe width="${videoWidth}" height="${videoHeight}" class="padding" src="${soundInfo[i].youtubeUrl}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     `,
         showCloseButton: true,
         padding: `${alertPadding}rem`,
@@ -185,7 +202,7 @@ soundboardApp.showFocusOutline = function () {
 //function that removes the current filter when sound has ended
 soundboardApp.removeCurrentFilter = function (i) {
     $sounds.on("ended", function () {
-        $(`.button__item--${i + 1}`).removeClass("filter");
+        $(`.button__item--${i + 1}`).children(".button__side").removeClass("filter");
     });
 }
 
@@ -196,13 +213,11 @@ soundboardApp.setupOnClickListeners = function () {
         $(`.button__item--${i + 1}`).on("click", function () {
             if ($sounds[i].paused) {
                 $sounds[i].play();
-                // $(this).css({ "filter": "grayscale(62%)" });
-                $(this).toggleClass("filter");
+                $(this).children(".button__side").toggleClass("filter");
             }
             else {
                 $sounds[i].pause();
-                // $(this).css({ "filter": "none" });
-                $(this).toggleClass("filter");
+                $(this).children(".button__side").toggleClass("filter");
             }
         });
         soundboardApp.removeCurrentFilter(i);
@@ -214,7 +229,7 @@ soundboardApp.setupOnClickListeners = function () {
 soundboardApp.removeExtraFilters = function () {
     $buttons.on("click", function () {
         $buttons.not($(this)).each(function (index, button) {
-            $(button).removeClass("filter");
+            $(button).children(".button__side").removeClass("filter");
         });
     });
 };
